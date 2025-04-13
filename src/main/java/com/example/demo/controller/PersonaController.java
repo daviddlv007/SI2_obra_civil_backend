cbf53255f8a09a585840bb2da8ed9177c86d4d0a
daviddlv007 <davidlanzadlv7@gmail.com>
2025-04-13 19:37:01 -0400

    Primer commit

--- src/main/java/com/example/demo/controller/PersonaController.java
+++ src/main/java/com/example/demo/controller/PersonaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/personass")
+@RequestMapping("/personas")
 public class PersonaController {
 
     private final PersonaService personaService;
