7bee55c505bf41d7f419cae512b61d2b4a3d831e
Alex Orellana <lexorel88@gmail.com>
2025-05-05 16:17:20 -0400

    Listar obras civiles desc

--- src/main/java/com/example/demo/repository/ObraCivilRepository.java
+++ src/main/java/com/example/demo/repository/ObraCivilRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.ObraCivil;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ObraCivilRepository extends JpaRepository<ObraCivil, Long> {
+
+    // Listar todas las obras civiles y ordenarlos por id de manera descendente o ascendente
+    List<ObraCivil> findAll(Sort sort);
 }
